cee30628f2542615f3de546683ad2f89f50a2de1
MAhmo0D9 <mahmoodpc2299@gmail.com>
2022-01-25 01:33:23 +0330

    Debug Csv Reader

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,52 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Iterator;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
         Scanner scanner;
         String filePath;
-        Scanner csvStreamer;
-        ArrayList<TransactionDetails> transactionList = new ArrayList<TransactionDetails>();
+
+        ArrayList<TransactionDetails> transactionList = new ArrayList<>();
 
 
 
         scanner = new Scanner(System.in);
         filePath = scanner.nextLine();
-        csvStreamer = new Scanner(filePath);
-        csvStreamer.useDelimiter(",");
+        Scanner csvStreamer = new Scanner(new File(filePath));
+//        csvStreamer.useDelimiter(",");
+
+//        while (csvStreamer.hasNext()){
+//
+//            System.out.println(csvStreamer.next());
+//            System.out.println("");
+//        }
 
         while(csvStreamer.hasNext()){
             TransactionDetails tempTransaction = new TransactionDetails();
-            tempTransaction.setRow(csvStreamer.nextLine());
-            tempTransaction.setTransactionDate(csvStreamer.nextLine());
-            tempTransaction.setTransactionHour(csvStreamer.nextLine());
-            tempTransaction.setTransactionType(csvStreamer.nextLine());
-            tempTransaction.setTerminalType(csvStreamer.nextLine());
-            tempTransaction.setPrice(csvStreamer.nextLine());
-            tempTransaction.setDisNumber(csvStreamer.nextLine());
-            tempTransaction.setLandDate(csvStreamer.nextLine());
-            tempTransaction.setTerminalNumber(csvStreamer.nextLine());
-            tempTransaction.setReferCode(csvStreamer.nextLine());
+            tempTransaction.setRow(csvStreamer.next());
+            tempTransaction.setTransactionDate(csvStreamer.next());
+            tempTransaction.setTransactionHour(csvStreamer.next());
+            tempTransaction.setTransactionType(csvStreamer.next());
+            tempTransaction.setTerminalType(csvStreamer.next());
+            tempTransaction.setPrice(csvStreamer.next());
+            tempTransaction.setDisNumber(csvStreamer.next());
+            tempTransaction.setLandDate(csvStreamer.next());
+            tempTransaction.setTerminalNumber(csvStreamer.next());
+            tempTransaction.setReferCode(csvStreamer.next());
             transactionList.add(tempTransaction);
             }
 
         Iterator<TransactionDetails> it = transactionList.iterator();
-        if(it.hasNext()){
-
-            System.out.printf("%s \t", it.next().getRow());
-            System.out.printf("%s \n",it.next().getPrice());
+        while(it.hasNext()){
+            TransactionDetails temp = it.next();
+            System.out.printf("%s \t", temp.getRow());
+            System.out.printf("%s \n", temp.getPrice());
         }
+        System.out.println("there is total: "+ transactionList.size() + " Transactions");
 
     }
 }
